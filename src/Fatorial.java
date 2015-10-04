360a4f90e39e4165fa4e57f44861ef943ca016c7
Guilherme Oliveira <gostantos@inf.ufrgs.br>
2015-10-03 22:12:06 -0300

    adding implementations

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,4 +1,11 @@
-
 public class Fatorial {
+
+	public int calc(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * calc(n - 1);
+		}
+	}
 
 }
--- src/FatorialTest.java
+++ src/FatorialTest.java
@@ -1,15 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FatorialTest {
 	@Test
 	public void calcFatorialTest() {
 		Fatorial fatorial = new Fatorial();
+
+		// equals asserts
 		Assert.assertEquals(120, fatorial.calc(5));
 		Assert.assertEquals(24, fatorial.calc(4));
 		Assert.assertEquals(720, fatorial.calc(6));
-		
+
+		// not equals asserts
 		Assert.assertNotEquals(1, fatorial.calc(6));
 		Assert.assertNotEquals(720, fatorial.calc(20));
-		Assert.assertEquals(0, fatorial.calc(0));
+		Assert.assertNotEquals(0, fatorial.calc(0));
 	}
 }
